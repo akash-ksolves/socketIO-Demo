88228be139a5738d62a365675fe49e8f2b955044
akash-ksolves <akash.kharat@ksolves.com>
2023-06-02 10:26:01 +0530

    modified user messages logic with specific to room

--- index.js
+++ index.js
@@ -23,27 +23,33 @@ const PORT = process.env.PORT || 8088;
 io.on("connection", (socket) => {
   console.log("WebSocket connected");
 
-  socket.on("create", () => {
-    // There is no arguments for creating room as of now, but we can provide room name as per client side requests
-    socket.join("room"); //here room is passed as a room name we can make it dynamic also
-  });
+  const room = (Math.random() + 1).toString(36).substring(7);
+
+  socket.join(room);
+
+  // socket.on("create", (room) => {
+  //   // There is no arguments for creating room as of now, but we can provide room name as per client side requests
+  //   socket.join(room); //here room is passed as a room name we can make it dynamic also
+  // });
 
   // Handle WebSocket messages
   socket.on("message", (message) => {
     console.log(`Received message: ${message}`);
+    // socket.to(room).emit("message", message);
 
     // to normal users
     // socket.broadcast.emit("message", message);
     // io.emit("message", message);
 
     // to only in rooms
-    io.sockets.in("room").emit("message", message);
+    io.sockets.in(room).emit("message", message);
     // console.log("rooms", io.sockets.adapter.rooms);
     // console.log(io.sockets.adapter.rooms.get(socket.id));
   });
 
   // Handle WebSocket disconnections
   socket.on("disconnect", () => {
+    socket.leave(room); // Remove the socket from the room
     console.log("WebSocket disconnected");
   });
 });
