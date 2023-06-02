f45f0025ec26e4f83422f13cc6ab62642e8d6550
akash-ksolves <akash.kharat@ksolves.com>
2023-06-02 18:06:57 +0530

    code refactor

--- index.js
+++ index.js
@@ -17,7 +17,6 @@ const https = require("https").createServer(secureOptions, app);
 const io = require("socket.io")(https, corsOptions);
 
 const PORT = process.env.PORT || 8088;
-
 // Handle WebSocket connections
 
 io.on("connection", (socket) => {
@@ -27,24 +26,9 @@ io.on("connection", (socket) => {
 
   socket.join(room);
 
-  // socket.on("create", (room) => {
-  //   // There is no arguments for creating room as of now, but we can provide room name as per client side requests
-  //   socket.join(room); //here room is passed as a room name we can make it dynamic also
-  // });
-
   // Handle WebSocket messages
   socket.on("message", (message) => {
-    console.log(`Received message: ${message}`);
-    // socket.to(room).emit("message", message);
-
-    // to normal users
-    // socket.broadcast.emit("message", message);
-    // io.emit("message", message);
-
-    // to only in rooms
     io.sockets.in(room).emit("message", message);
-    // console.log("rooms", io.sockets.adapter.rooms);
-    // console.log(io.sockets.adapter.rooms.get(socket.id));
   });
 
   // Handle WebSocket disconnections
